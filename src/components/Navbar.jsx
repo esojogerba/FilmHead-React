8d113dfbc3bfcbeb81ba2b4c4db3d997548f8e73
Jose Abrego <joseabrego13@gmail.com>
2025-06-26 17:47:53 -0500

    Implemented navbar functionality, added images.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import navbarLogo from "../assets/images/filmhead-nav-logo.svg";
 import logo from "../assets/images/FilmHead-logo.png";
+import hamburgerIcon from "../assets/images/icon-hamburger.svg";
+import closeIcon from "../assets/images/icon-close.svg";
 
 const Navbar = () => {
+    const [navVisible, setNavVisible] = useState(false);
+
+    const toggleNav = () => {
+        setNavVisible((prev) => !prev);
+    };
+
     return (
         <header className="container primary-header">
             <div>
@@ -14,11 +22,13 @@ const Navbar = () => {
                     />
                 </a>
             </div>
+
             <div>
                 <a href="index.html" className="mobile-nav-logo">
                     <img src={logo} alt="FilmHead logo" id="nav-logo-img" />
                 </a>
             </div>
+
             <div className="search-box-mobile">
                 <input
                     className="search-field"
@@ -26,14 +36,30 @@ const Navbar = () => {
                     placeholder="Search..."
                 />
             </div>
+
             <button
                 className="mobile-nav-toggle"
                 aria-controls="primary-navigation"
-                aria-expanded="false"
+                aria-expanded={navVisible}
+                onClick={toggleNav}
+                style={{
+                    backgroundImage: `url(${
+                        navVisible ? closeIcon : hamburgerIcon
+                    })`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "contain", // optional but helps with scaling
+                    border: "none",
+                    backgroundColor: "transparent",
+                    width: "2rem",
+                    aspectRatio: "1 / 1",
+                    zIndex: 9999,
+                }}
             >
                 <span className="sr-only">Menu</span>
             </button>
-            <nav className="nav-links" id="nav-links" data-visible="false">
+
+            <nav className="nav-links" id="nav-links" data-visible={navVisible}>
                 <ul role="list" className="nav-links-left">
                     <li>
                         <a href="/html/movies.html">Movies</a>
@@ -54,12 +80,12 @@ const Navbar = () => {
                         />
                     </li>
                     <li>
-                        <a href="html/sign-up.html" id="nav-link-sign-up">
+                        <a href="/html/sign-up.html" id="nav-link-sign-up">
                             Sign Up
                         </a>
                     </li>
                     <li>
-                        <a href="html/log-in.html">Log In</a>
+                        <a href="/html/log-in.html">Log In</a>
                     </li>
                 </ul>
             </nav>
