3879fa496b3b118e90da9d70222a4e3aae6c94a3
Jose Abrego <joseabrego13@gmail.com>
2025-06-30 18:20:24 -0500

    Added movies page routing.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
 
 // TODO: Fix router to match RC2024.
 
@@ -14,6 +15,7 @@ const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
         </Route>
     )
 );
