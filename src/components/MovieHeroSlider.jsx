4a93c57dd6f9d8765d651bdeae8990e1b817e06f
Jose Abrego <joseabrego13@gmail.com>
2025-07-05 20:55:29 -0500

    Added MovieHeroSliderItem component.

--- src/components/MovieHeroSlider.jsx
+++ src/components/MovieHeroSlider.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import MovieHeroSliderItem from "./MovieHeroSliderItem";
 import bannerImg from "../assets/images/Blade Runner Banner.png";
 import posterImg from "../assets/images/Blade Runner Poster.png";
+import { imageBaseURL, API_KEY, fetchDataFromAPI } from "../utils/api";
 
 // TODO: API integration
 // TODO: slider timer
@@ -8,101 +11,74 @@ import posterImg from "../assets/images/Blade Runner Poster.png";
 // TODO: add to folder pop up integration
 
 const MovieHeroSlider = () => {
+    const [movies, setMovies] = useState([]);
+    const [genres, setGenres] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`;
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                console.log(data);
+                setMovies(data.results);
+            } catch (error) {
+                console.log("Error fetching data", data);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    useEffect(() => {
+        // Fetch all genres. Example: [ { "id": "123", "name": "Action" } ]
+        // Then change genre format to {123: "Action"}
+        const genreList = {
+            // Assign correct genre string to each genre_id provided. Example: [23 , 43] = "Action, Romance".
+            asString(genreIdList) {
+                // Will hold list of genre strings.
+                let newGenreList = [];
+
+                for (const genreId of genreIdList) {
+                    // If current genreId exists in genreList, push it to newGenreList.
+                    // this == genreList
+                    this[genreId] && newGenreList.push(this[genreId]);
+                }
+                return newGenreList.join(" · ");
+            },
+        };
+
+        const fetchGenres = async () => {
+            const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+
+                for (const { id, name } of data.genres) {
+                    genreList[id] = name;
+                }
+                console.log(genreList);
+                setGenres(genreList);
+            } catch (error) {
+                console.log("Error fetching data", data);
+            }
+        };
+
+        fetchGenres();
+    }, []);
+
     return (
         <section className="banner">
             <div className="banner-slider-row">
-                <div className="banner-slider-item active">
-                    <img
-                        src={bannerImg}
-                        alt=""
-                        className="img-cover"
-                        title="Blade Runner"
-                        loading="eager"
+                {movies.map((movie, i) => (
+                    <MovieHeroSliderItem
+                        key={movie.id}
+                        index={i}
+                        movie={movie}
+                        imageBaseURL={imageBaseURL}
+                        genres={genres}
                     />
-                    <div className="banner-content">
-                        <h2 className="banner-heading">Blade Runner</h2>
-                        <p className="banner-text">
-                            In the smog-choked dystopian Los Angeles of 2019,
-                            blade runner Rick Deckard is called out of
-                            retirement to terminate a quartet of replicants who
-                            have escaped to Earth seeking their creator for a
-                            way to extend their short life spans.
-                        </p>
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                            <div className="meta-item">1hr 58min</div>
-                            <div className="meta-item card-badge">R</div>
-                        </div>
-                        <p className="banner-genre">
-                            Science Fiction · Drama · Thriller
-                        </p>
-                        <div className="banner-buttons">
-                            <a
-                                className="btn"
-                                href="movie-detials.html"
-                                onclick=""
-                            >
-                                Details
-                            </a>
-                            <a
-                                className="btn-icon"
-                                href=""
-                                onclick=""
-                                id="banner-add-btn"
-                            >
-                                <svg className="material-icon" id="add-svg">
-                                    <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="banner-slider-item">
-                    <img
-                        src={bannerImg}
-                        alt=""
-                        className="img-cover"
-                        title="Blade Runner"
-                        loading="eager"
-                    />
-                    <div className="banner-content">
-                        <h2 className="banner-heading">Blade Runner</h2>
-                        <p className="banner-text">
-                            In the smog-choked dystopian Los Angeles of 2019,
-                            blade runner Rick Deckard is called out of
-                            retirement to terminate a quartet of replicants who
-                            have escaped to Earth seeking their creator for a
-                            way to extend their short life spans.
-                        </p>
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                            <div className="meta-item">1hr 58min</div>
-                            <div className="meta-item card-badge">R</div>
-                        </div>
-                        <p className="banner-genre">
-                            Science Fiction · Drama · Thriller
-                        </p>
-                        <div className="banner-buttons">
-                            <a
-                                className="btn"
-                                href="movie-detials.html"
-                                onclick=""
-                            >
-                                Details
-                            </a>
-                            <a
-                                className="btn-icon"
-                                href=""
-                                onclick=""
-                                id="banner-add-btn"
-                            >
-                                <svg className="material-icon" id="add-svg">
-                                    <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                                </svg>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
             <div className="banner-control-row">
                 <div className="banner-control-inner">
