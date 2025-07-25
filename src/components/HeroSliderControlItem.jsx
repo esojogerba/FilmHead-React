84a2cc2b95a33ecb22361b65ee3349586f23bdb5
Jose Abrego <joseabrego13@gmail.com>
2025-07-24 22:01:19 -0500

    Hero slider implementation. Automatic slide changes and manual slide select.

--- src/components/HeroSliderControlItem.jsx
+++ src/components/HeroSliderControlItem.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import posterImg from "../assets/images/Blade Runner Poster.png";
 
-const HeroSliderControlItem = ({ index, movie, imageBaseURL }) => {
+const HeroSliderControlItem = ({
+    index,
+    movie,
+    imageBaseURL,
+    onClick,
+    isActive,
+}) => {
     return (
-        <button className="banner-control-item poster-box active">
+        <button
+            className={`banner-control-item poster-box ${
+                isActive ? "active" : ""
+            }`} // Add active class
+            onClick={onClick} // Trigger slide switch
+        >
             <img
                 src={`${imageBaseURL}w154${movie.poster_path}`}
-                alt="Blade Runner"
+                alt={movie.title}
                 loading="lazy"
                 draggable="false"
                 className="img-cover"
