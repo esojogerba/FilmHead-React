81ce4efe594de525b8d9f8cbec1796a6501e6009
Jose Abrego <joseabrego13@gmail.com>
2025-07-31 14:04:55 -0500

    Moved genre fetch to MoviesPage for reusability.

--- src/components/MovieHeroSlider.jsx
+++ src/components/MovieHeroSlider.jsx
@@ -9,9 +9,8 @@ import HeroSliderControl from "./HeroSliderControl";
 // TODO: Routing to details page
 // TODO: add to folder pop up integration
 
-const MovieHeroSlider = () => {
+const MovieHeroSlider = ({ genres }) => {
     const [movies, setMovies] = useState([]);
-    const [genres, setGenres] = useState({});
 
     const [activeIndex, setActiveIndex] = useState(0);
     const intervalRef = useRef(null);
@@ -30,43 +29,6 @@ const MovieHeroSlider = () => {
         };
 
         fetchMovies();
-    }, []);
-
-    useEffect(() => {
-        // Fetch all genres. Example: [ { "id": "123", "name": "Action" } ]
-        // Then change genre format to {123: "Action"}
-        const genreList = {
-            // Assign correct genre string to each genre_id provided. Example: [23 , 43] = "Action, Romance".
-            asString(genreIdList) {
-                // Will hold list of genre strings.
-                let newGenreList = [];
-
-                for (const genreId of genreIdList) {
-                    // If current genreId exists in genreList, push it to newGenreList.
-                    // this == genreList
-                    this[genreId] && newGenreList.push(this[genreId]);
-                }
-                return newGenreList.join(" · ");
-            },
-        };
-
-        const fetchGenres = async () => {
-            const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
-            try {
-                const res = await fetch(apiUrl);
-                const data = await res.json();
-
-                for (const { id, name } of data.genres) {
-                    genreList[id] = name;
-                }
-                console.log(genreList);
-                setGenres(genreList);
-            } catch (error) {
-                console.log("Error fetching data", data);
-            }
-        };
-
-        fetchGenres();
     }, []);
 
     const startAutoSlide = () => {
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,13 +1,54 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { imageBaseURL, API_KEY, fetchDataFromAPI } from "../utils/api";
 import bannerImg from "../assets/images/Blade Runner Banner.png";
 import posterImg from "../assets/images/Blade Runner Poster.png";
 import MovieHeroSlider from "../components/MovieHeroSlider";
 import MediaPageHeader from "../components/MediaPageHeader";
 
 const MoviesPage = () => {
+    const [genres, setGenres] = useState({});
+
+    useEffect(() => {
+        // Fetch all genres. Example: [ { "id": "123", "name": "Action" } ]
+        // Then change genre format to {123: "Action"}
+        const genreList = {
+            // Assign correct genre string to each genre_id provided. Example: [23 , 43] = "Action, Romance".
+            asString(genreIdList) {
+                // Will hold list of genre strings.
+                let newGenreList = [];
+
+                for (const genreId of genreIdList) {
+                    // If current genreId exists in genreList, push it to newGenreList.
+                    // this == genreList
+                    this[genreId] && newGenreList.push(this[genreId]);
+                }
+                return newGenreList.join(" · ");
+            },
+        };
+
+        const fetchGenres = async () => {
+            const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+
+                for (const { id, name } of data.genres) {
+                    genreList[id] = name;
+                }
+                console.log(genreList);
+                setGenres(genreList);
+            } catch (error) {
+                console.log("Error fetching data", data);
+            }
+        };
+
+        fetchGenres();
+    }, []);
+
     return (
         <article page-content="">
-            <MovieHeroSlider />
+            <MovieHeroSlider genres={genres} />
             <article className="container">
                 <MediaPageHeader />
                 <section className="media-scroll">
