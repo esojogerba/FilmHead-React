5c3c13773260c11892000a270c80daab4d523789
Jose Abrego <joseabrego13@gmail.com>
2025-08-18 20:29:15 -0500

    Added streaming, rent, and buy options for media.

--- src/components/AvailableOn.jsx
+++ src/components/AvailableOn.jsx
@@ -1,75 +1,94 @@
 import React from "react";
 import huluLogo from "../assets/images/hulu-logo.jpg";
 
-const AvailableOn = () => {
+const AvailableOn = ({ availableOn, imageBaseURL }) => {
+    let inUS = false;
+    let watchResults = [];
+
+    if ("US" in availableOn) {
+        inUS = true;
+        watchResults = availableOn.US;
+    } else {
+        inUS = false;
+    }
+
     return (
         <section className="media-scroll container">
             <div className="media-scroll-title-wrapper">
                 <h3 className="media-scroll-title">Available On (US)</h3>
             </div>
+
             <div className="media-slider-list">
                 <div className="slider-list-inner watch-col">
-                    <div className="watch-stream-col">
-                        <h4 className="watch-header">Stream</h4>
-                        <div className="watch-platforms-row">
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                        </div>
-                    </div>
-                    <div className="watch-rent-col">
-                        <h4 className="watch-header">Rent</h4>
-                        <div className="watch-platforms-row">
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                        </div>
-                    </div>
-                    <div className="watch-buy-col">
-                        <h4 className="watch-header">Buy</h4>
-                        <div className="watch-platforms-row">
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                            <img
-                                className="watch-logo"
-                                src={huluLogo}
-                                alt="Hulu"
-                            />
-                        </div>
-                    </div>
-                    <h4 className="watch-header">Not Available in US</h4>
+                    {inUS ? (
+                        <>
+                            {"flatrate" in watchResults ? (
+                                <div className="watch-stream-col">
+                                    <h4 className="watch-header">Stream</h4>
+                                    <div className="watch-platforms-row">
+                                        {watchResults.flatrate.map((entry) => (
+                                            <img
+                                                key={entry.provider_id}
+                                                className="watch-logo"
+                                                src={
+                                                    imageBaseURL +
+                                                    "w92" +
+                                                    entry.logo_path
+                                                }
+                                                alt={entry.provider_name}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                            {"rent" in watchResults ? (
+                                <div className="watch-rent-col">
+                                    <h4 className="watch-header">Rent</h4>
+                                    <div className="watch-platforms-row">
+                                        {watchResults.rent.map((entry) => (
+                                            <img
+                                                key={entry.provider_id}
+                                                className="watch-logo"
+                                                src={
+                                                    imageBaseURL +
+                                                    "w92" +
+                                                    entry.logo_path
+                                                }
+                                                alt={entry.provider_name}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                            {"buy" in watchResults ? (
+                                <div className="watch-buy-col">
+                                    <h4 className="watch-header">Buy</h4>
+                                    <div className="watch-platforms-row">
+                                        {watchResults.buy.map((entry) => (
+                                            <img
+                                                key={entry.provider_id}
+                                                className="watch-logo"
+                                                src={
+                                                    imageBaseURL +
+                                                    "w92" +
+                                                    entry.logo_path
+                                                }
+                                                alt={entry.provider_name}
+                                            />
+                                        ))}
+                                    </div>
+                                </div>
+                            ) : (
+                                ""
+                            )}
+                        </>
+                    ) : (
+                        <h4 className="watch-header">Not Available in US</h4>
+                    )}
                 </div>
             </div>
         </section>
