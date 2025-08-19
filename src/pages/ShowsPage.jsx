aa2273ddd3b71bd4c99832ee4e2a34736b4717c9
Jose Abrego <joseabrego13@gmail.com>
2025-08-19 18:32:18 -0500

    Fetched genres from API.

--- src/pages/ShowsPage.jsx
+++ src/pages/ShowsPage.jsx
@@ -1,6 +1,48 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { imageBaseURL, API_KEY, fetchDataFromAPI } from "../utils/api";
 
 const ShowsPage = () => {
+    const [genres, setGenres] = useState({});
+
+    useEffect(() => {
+        // Fetch all genres. Example: [ { "id": "123", "name": "Action" } ]
+        // Then change genre format to {123: "Action"}
+        const genreList = {
+            // Assign correct genre string to each genre_id provided. Example: [23 , 43] = "Action, Romance".
+            asString(genreIdList) {
+                // Will hold list of genre strings.
+                let newGenreList = [];
+
+                for (const genreId of genreIdList) {
+                    // If current genreId exists in genreList, push it to newGenreList.
+                    // this == genreList
+                    this[genreId] && newGenreList.push(this[genreId]);
+                }
+                return newGenreList.join(" · ");
+            },
+        };
+
+        const fetchGenres = async () => {
+            const apiUrl = `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`;
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+
+                for (const { id, name } of data.genres) {
+                    genreList[id] = name;
+                }
+                setGenres(genreList);
+            } catch (error) {
+                console.log("Error fetching data", data);
+            }
+        };
+
+        fetchGenres();
+    }, []);
+
+    console.log(genres);
+
     return <div>ShowsPage</div>;
 };
 
