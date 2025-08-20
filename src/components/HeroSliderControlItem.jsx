95321adcfc13d34b162baa73e0183cf831b618c4
Jose Abrego <joseabrego13@gmail.com>
2025-08-20 18:55:08 -0500

    Component now works for both movies and shows.

--- src/components/HeroSliderControlItem.jsx
+++ src/components/HeroSliderControlItem.jsx
@@ -3,27 +3,47 @@ import posterImg from "../assets/images/Blade Runner Poster.png";
 
 const HeroSliderControlItem = ({
     index,
-    movie,
+    media,
     imageBaseURL,
     onClick,
     isActive,
+    type,
 }) => {
-    return (
-        <button
-            className={`banner-control-item poster-box ${
-                isActive ? "active" : ""
-            }`} // Add active class
-            onClick={onClick} // Trigger slide switch
-        >
-            <img
-                src={`${imageBaseURL}w154${movie.poster_path}`}
-                alt={movie.title}
-                loading="lazy"
-                draggable="false"
-                className="img-cover"
-            />
-        </button>
-    );
+    if (type === "movie") {
+        return (
+            <button
+                className={`banner-control-item poster-box ${
+                    isActive ? "active" : ""
+                }`} // Add active class
+                onClick={onClick} // Trigger slide switch
+            >
+                <img
+                    src={`${imageBaseURL}w154${media.poster_path}`}
+                    alt={media.title}
+                    loading="lazy"
+                    draggable="false"
+                    className="img-cover"
+                />
+            </button>
+        );
+    } else if (type === "show") {
+        return (
+            <button
+                className={`banner-control-item poster-box ${
+                    isActive ? "active" : ""
+                }`} // Add active class
+                onClick={onClick} // Trigger slide switch
+            >
+                <img
+                    src={`${imageBaseURL}w154${media.poster_path}`}
+                    alt={media.name}
+                    loading="lazy"
+                    draggable="false"
+                    className="img-cover"
+                />
+            </button>
+        );
+    }
 };
 
 export default HeroSliderControlItem;
