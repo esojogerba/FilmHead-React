294d51351313fb587498e29c05a3ec4bc52fb7d0
Jose Abrego <joseabrego13@gmail.com>
2025-08-20 18:55:03 -0500

    Component now works for both movies and shows.

--- src/components/HeroSliderControl.jsx
+++ src/components/HeroSliderControl.jsx
@@ -3,21 +3,23 @@ import { useState, useEffect } from "react";
 import HeroSliderControlItem from "./HeroSliderControlItem";
 
 const HeroSliderControl = ({
-    movies,
+    mediaList,
     imageBaseURL,
     activeIndex,
     handleControlClick,
+    type,
 }) => {
     return (
         <div className="banner-control-row">
             <div className="banner-control-inner">
-                {movies.map((movie, i) => (
+                {mediaList.map((media, i) => (
                     <HeroSliderControlItem
-                        key={movie.id}
+                        key={media.id}
                         index={i}
-                        movie={movie}
+                        media={media}
                         imageBaseURL={imageBaseURL}
                         isActive={i === activeIndex}
+                        type={type}
                         onClick={() => handleControlClick(i)}
                     />
                 ))}
