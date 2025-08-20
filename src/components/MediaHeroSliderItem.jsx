21a9fb6000189903ac96dbd6c261290da7ec59bd
Jose Abrego <joseabrego13@gmail.com>
2025-08-20 18:54:47 -0500

    Renamed component, now works for both movies and shows.

---
+++ src/components/MediaHeroSliderItem.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// TODO: Implement details button
+// TODO: Implement add button
+
+const MediaHeroSliderItem = ({
+    index,
+    media,
+    imageBaseURL,
+    genres,
+    isActive,
+    type,
+}) => {
+    if (type === "movie") {
+        return (
+            <div
+                className={
+                    isActive
+                        ? "banner-slider-item active"
+                        : "banner-slider-item"
+                }
+            >
+                <img
+                    src={`${imageBaseURL}w1280${media.backdrop_path}`}
+                    alt={media.title}
+                    className="img-cover"
+                    title={media.title}
+                    loading={index === 0 ? "eager" : "lazy"}
+                />
+
+                <div className="banner-content">
+                    <h2 className="banner-heading">{media.title}</h2>
+                    <p className="banner-text">{media.overview}</p>
+                    <div className="meta-list">
+                        <div className="meta-item">
+                            {media.release_date.split("-")[0]}
+                        </div>
+                    </div>
+                    <p className="banner-genre">
+                        {genres?.asString?.(media.genre_ids) || ""}
+                    </p>
+                    <div className="banner-buttons">
+                        <Link to={`/details/movie/${media.id}`} className="btn">
+                            Details
+                        </Link>
+                        <a
+                            className="btn-icon"
+                            href=""
+                            onClick={() => null}
+                            id="banner-add-btn"
+                        >
+                            <svg className="material-icon" id="add-svg">
+                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    } else if (type === "show") {
+        return (
+            <div
+                className={
+                    isActive
+                        ? "banner-slider-item active"
+                        : "banner-slider-item"
+                }
+            >
+                <img
+                    src={`${imageBaseURL}w1280${media.backdrop_path}`}
+                    alt={media.name}
+                    className="img-cover"
+                    title={media.name}
+                    loading={index === 0 ? "eager" : "lazy"}
+                />
+
+                <div className="banner-content">
+                    <h2 className="banner-heading">{media.name}</h2>
+                    <p className="banner-text">{media.overview}</p>
+                    <div className="meta-list">
+                        <div className="meta-item">
+                            {media.first_air_date.split("-")[0]}
+                        </div>
+                    </div>
+                    <p className="banner-genre">
+                        {genres?.asString?.(media.genre_ids) || ""}
+                    </p>
+                    <div className="banner-buttons">
+                        <Link to={`/details/movie/${media.id}`} className="btn">
+                            Details
+                        </Link>
+                        <a
+                            className="btn-icon"
+                            href=""
+                            onClick={() => null}
+                            id="banner-add-btn"
+                        >
+                            <svg className="material-icon" id="add-svg">
+                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default MediaHeroSliderItem;
