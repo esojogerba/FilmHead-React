a573d1a3e3e3db7ea78c54db4ce80cce82271b11
Jose Abrego <joseabrego13@gmail.com>
2025-09-03 17:31:31 -0500

    Created log in page.

---
+++ src/pages/LogInPage.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import screening from "../assets/images/screening.svg";
+import bgCircle1 from "../assets/images/bg-circle-1.png";
+import bgCircle2 from "../assets/images/bg-circle-2.png";
+
+const LogInPage = () => {
+    return (
+        <main className="mobile-bg-img | padding-bottom-900-mobile">
+            <section className="account-page text-center-sm-only margin-bottom-500">
+                <div className="container account-form-bg-circles">
+                    <div className="account-form-columns">
+                        <div className="account-form-col-left">
+                            <div className="account-form div-center-sm-only">
+                                <h1 className="account-form-header">Log In</h1>
+                                <form
+                                    action=""
+                                    className="sign-up-form div-center-sm-only"
+                                >
+                                    <div className="input-group">
+                                        <input
+                                            type="text"
+                                            id="username"
+                                            name="username"
+                                            autoComplete="off"
+                                            required
+                                        />
+                                        <label htmlFor="username">
+                                            Username
+                                        </label>
+                                    </div>
+                                    <div className="input-group">
+                                        <input
+                                            type="password"
+                                            id="pw"
+                                            name="pw"
+                                            autoComplete="off"
+                                            required
+                                        />
+                                        <label htmlFor="pw">Password</label>
+                                    </div>
+                                </form>
+                                <button className="btn account-form-btn">
+                                    Let's Go
+                                </button>
+                            </div>
+                        </div>
+                        <div className="account-form-col-right">
+                            <img
+                                src={screening}
+                                alt="screening"
+                                className="form-image"
+                                loading="eager"
+                            />
+                        </div>
+                    </div>
+                    <img
+                        className="account-form-bg-circle-top"
+                        src={bgCircle1}
+                        alt="background decorative circle"
+                    />
+                    <img
+                        className="account-form-bg-circle-bottom"
+                        src={bgCircle2}
+                        alt="background decorative circle"
+                    />
+                </div>
+            </section>
+            <div className="pop-up-overlay" />
+            <div className="pop-up-overlay second-overlay" />
+        </main>
+    );
+};
+
+export default LogInPage;
