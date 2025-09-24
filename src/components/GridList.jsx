164529a34d29bd1079a0a87742e524e5ca0978ed
Jose Abrego <joseabrego13@gmail.com>
2025-09-24 02:45:17 -0500

    Populated GridList. Created media cards for all items in mediaList.

--- src/components/GridList.jsx
+++ src/components/GridList.jsx
@@ -1,103 +1,26 @@
 import React from "react";
-import posterImg from "../assets/images/Blade Runner Poster.png";
+import MediaCard from "./MediaCard";
 
-const GridList = () => {
-    return (
-        <>
-            <div className="grid-list">
-                <div className="grid-card">
-                    <figure className="poster-box grid-card-poster">
-                        <img
-                            src={posterImg}
-                            alt="Blade Runner"
-                            className="img-cover"
-                            loading="lazy"
-                        />
-                        <a href="" className="grid-card-add-btn">
-                            <svg className="material-icon" id="card-add-svg">
-                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                            </svg>
-                        </a>
-                    </figure>
-                    <h4 className="media-card-title">Blade Runner</h4>
-                    <div className="meta-list media-card-meta">
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                        </div>
-                    </div>
-                    <a href="" className="card-btn" title="" onClick={null}></a>
-                </div>
-                <div className="grid-card">
-                    <figure className="poster-box grid-card-poster">
-                        <img
-                            src={posterImg}
-                            alt="Blade Runner"
-                            className="img-cover"
-                            loading="lazy"
-                        />
-                        <a href="" className="grid-card-add-btn">
-                            <svg className="material-icon" id="card-add-svg">
-                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                            </svg>
-                        </a>
-                    </figure>
-                    <h4 className="media-card-title">Blade Runner</h4>
-                    <div className="meta-list media-card-meta">
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                        </div>
-                    </div>
-                    <a href="" className="card-btn" title="" onClick={null}></a>
-                </div>
-                <div className="grid-card">
-                    <figure className="poster-box grid-card-poster">
-                        <img
-                            src={posterImg}
-                            alt="Blade Runner"
-                            className="img-cover"
-                            loading="lazy"
-                        />
-                        <a href="" className="grid-card-add-btn">
-                            <svg className="material-icon" id="card-add-svg">
-                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                            </svg>
-                        </a>
-                    </figure>
-                    <h4 className="media-card-title">Blade Runner</h4>
-                    <div className="meta-list media-card-meta">
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                        </div>
-                    </div>
-                    <a href="" className="card-btn" title="" onClick={null}></a>
-                </div>
-                <div className="grid-card">
-                    <figure className="poster-box grid-card-poster">
-                        <img
-                            src={posterImg}
-                            alt="Blade Runner"
-                            className="img-cover"
-                            loading="lazy"
-                        />
-                        <a href="" className="grid-card-add-btn">
-                            <svg className="material-icon" id="card-add-svg">
-                                <use xlinkHref="./assets/images/icons.svg#add-icon" />
-                            </svg>
-                        </a>
-                    </figure>
-                    <h4 className="media-card-title">Blade Runner</h4>
-                    <div className="meta-list media-card-meta">
-                        <div className="meta-list">
-                            <div className="meta-item">1982</div>
-                        </div>
-                    </div>
-                    <a href="" className="card-btn" title="" onClick={null}></a>
-                </div>
-            </div>
+const GridList = ({ mediaList, type }) => {
+    const mediaType = "grid-" + type;
 
-            <button className="btn load-more">Load More</button>
-        </>
-    );
+    if (mediaList != null) {
+        return (
+            <>
+                <div className="grid-list">
+                    {mediaList.map((media, i) => (
+                        <MediaCard
+                            key={media.id}
+                            mediaData={media}
+                            type={mediaType}
+                        />
+                    ))}
+                </div>
+
+                <button className="btn load-more">Load More</button>
+            </>
+        );
+    }
 };
 
 export default GridList;
