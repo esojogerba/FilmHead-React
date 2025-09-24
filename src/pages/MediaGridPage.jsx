4a619d94979773635f51afeb1b08dff26a1226f0
Jose Abrego <joseabrego13@gmail.com>
2025-09-23 19:14:11 -0500

    Retrieve necessary parameters from state.

--- src/pages/MediaGridPage.jsx
+++ src/pages/MediaGridPage.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import GridHeader from "../components/GridHeader";
 import GridList from "../components/GridList";
 
-// TODO: How to load in data not using parameters (so link isn't long)
 // TODO: Load title of page into GridHeader
 // TODO: Create a card for each movie in the list (inside grid-list)
 // TODO: Create a grid card for both movies and shows (inside MediaCard)
 // TODO: Button must load next set of movies (next page of the list it is loading)
 
 const MediaGridPage = () => {
+    const location = useLocation();
+    const [config, setConfig] = useState(() => {
+        // Try from location.state first, fallback to localStorage
+        return (
+            location.state ||
+            JSON.parse(localStorage.getItem("mediaGridConfig"))
+        );
+    });
+
+    useEffect(() => {
+        if (location.state) {
+            // If we navigated here with state, update localStorage too
+            localStorage.setItem(
+                "mediaGridConfig",
+                JSON.stringify(location.state)
+            );
+            setConfig(location.state);
+        }
+    }, [location.state]);
+
+    if (!config) {
+        return <p>No data found for this grid.</p>;
+    }
+
+    console.log(config);
+
     return (
         <main>
             <article page-content="">
