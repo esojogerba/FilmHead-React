d0adead0fe268714908db249ffac3de67252efe0
Jose Abrego <joseabrego13@gmail.com>
2025-09-29 04:43:55 -0500

    Implemented search function.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import SearchOverlay from "../components/SearchOverlay";
+import { SearchProvider } from "../contexts/SearchContext";
 
 const MainLayout = () => {
     return (
-        <>
+        <SearchProvider>
             <Navbar />
+            <SearchOverlay />
             <Outlet />
             <Footer />
-        </>
+        </SearchProvider>
     );
 };
 
