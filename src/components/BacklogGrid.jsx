0174b157395714f0925a23890a262da8e4a137a6
Jose Abrego <joseabrego13@gmail.com>
2025-10-02 02:45:03 -0500

    Added backlog grid component.

---
+++ src/components/BacklogGrid.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Folder from "./Folder";
+
+const BacklogGrid = () => {
+    return (
+        <div className="backlog-grid">
+            <Folder />
+            <Folder />
+            <Folder />
+            <Folder />
+            <Folder />
+        </div>
+    );
+};
+
+export default BacklogGrid;
