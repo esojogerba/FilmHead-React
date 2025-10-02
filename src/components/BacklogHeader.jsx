78f4d092216026023a97afd43219952aba8640a2
Jose Abrego <joseabrego13@gmail.com>
2025-10-02 02:44:45 -0500

    Created backlog header component.

---
+++ src/components/BacklogHeader.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const BacklogHeader = () => {
+    return (
+        <>
+            <div className="backlog-header">
+                <svg className="material-icon" id="backlog-svg">
+                    <use
+                        xlinkHref={`${
+                            import.meta.env.BASE_URL
+                        }assets/images/icons.svg#backlog`}
+                    />
+                </svg>
+                <h1 id="backlog-title">Backlog</h1>
+            </div>
+
+            <div className="backlog-menu">
+                <input
+                    className="backlog-search-field"
+                    type="search"
+                    placeholder="Search..."
+                />
+                <a className="btn-icon" onClick={null} id="backlog-add-btn">
+                    <svg className="material-icon" id="add-svg">
+                        <use
+                            xlinkHref={`${
+                                import.meta.env.BASE_URL
+                            }assets/images/icons.svg#add-icon`}
+                        />
+                    </svg>
+                </a>
+            </div>
+        </>
+    );
+};
+
+export default BacklogHeader;
