a3b934ff52836b06fe7a8991b06d2a2d3194f574
Jose Abrego <joseabrego13@gmail.com>
2025-10-02 02:45:14 -0500

    Created folder component.

---
+++ src/components/Folder.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import posterImg from "../assets/images/Blade Runner Poster.png";
+
+const Folder = () => {
+    return (
+        <div className="folder">
+            <div className="folder-posters">
+                <figure className="poster-box folder-poster">
+                    <img
+                        src={posterImg}
+                        alt="Blade Runner"
+                        className="img-cover"
+                        loading="lazy"
+                    />
+                </figure>
+                <figure className="poster-box folder-poster">
+                    <img
+                        src={posterImg}
+                        alt="Blade Runner"
+                        className="img-cover"
+                        loading="lazy"
+                    />
+                </figure>
+                <figure className="poster-box folder-poster">
+                    <img
+                        src={posterImg}
+                        alt="Blade Runner"
+                        className="img-cover"
+                        loading="lazy"
+                    />
+                </figure>
+            </div>
+            <div className="folder-details">
+                <h3 className="folder-title">Folder Name</h3>
+                <p className="folder-entry-count">15 Entries</p>
+            </div>
+            <a className="btn-icon" onClick={null} id="backlog-trash-btn">
+                <svg className="material-icon" id="trash-svg">
+                    <use
+                        xlinkHref={`${
+                            import.meta.env.BASE_URL
+                        }assets/images/icons.svg#trash`}
+                    />
+                </svg>
+            </a>
+            <a href="#" className="folder-btn" title="" onClick={null} />
+        </div>
+    );
+};
+
+export default Folder;
