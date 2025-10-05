4330c79dfb84308f728911d1f1ec00a0d8083900
Jose Abrego <joseabrego13@gmail.com>
2025-10-05 03:33:44 -0500

    Pop up can be opened and closed. Can open create folder pop up too.

--- src/components/AddToFolder.jsx
+++ src/components/AddToFolder.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import closeIcon from "../assets/images/icon-close.svg";
+import { usePopup } from "../contexts/PopupContext";
 
 const AddToFolder = () => {
+    const { activePopup, openPopup, closePopup } = usePopup();
+
+    if (activePopup !== "addToFolder") return null; // only show when active
+
     return (
         <>
-            <div className="pop-up-overlay active"></div>
+            <div className="pop-up-overlay active" onClick={closePopup}></div>
 
             <div className="add-to-folder active" aria-label="Add To Folder">
                 <div className="add-to-folder-header">
-                    <a className="pop-up-close-btn" onClick={null}>
+                    <a className="pop-up-close-btn" onClick={closePopup}>
                         <img
                             id="pop-up-close-img"
                             src={closeIcon}
@@ -24,7 +29,7 @@ const AddToFolder = () => {
                     <a
                         className="btn btn-text-icon"
                         id="add-new-folder-btn"
-                        onClick={null}
+                        onClick={() => openPopup("createFolder")}
                     >
                         <svg className="material-icon" id="add-to-folder-svg">
                             <use
@@ -42,41 +47,14 @@ const AddToFolder = () => {
                         <span>Folder</span>
                         <a className="add-to-folder-scroll-btn" href=""></a>
                     </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
-                    <div className="add-to-folder-scroll-item">
-                        <span>Folder</span>
-                        <a className="add-to-folder-scroll-btn" href=""></a>
-                    </div>
+                    {/* Repeat... */}
                 </div>
 
                 <div className="add-to-folder-footer">
                     <a
                         className="btn btn-text-icon"
                         id="add-to-folder-btn"
-                        onClick={null}
+                        onClick={closePopup}
                     >
                         <svg className="material-icon" id="add-to-folder-svg">
                             <use
