90cfd741a95085c700303dc81953aba6ce1d69b5
Jose Abrego <joseabrego13@gmail.com>
2025-10-05 03:34:28 -0500

    Added pop up context.

--- src/pages/BacklogPage.jsx
+++ src/pages/BacklogPage.jsx
@@ -3,20 +3,41 @@ import BacklogHeader from "../components/BacklogHeader";
 import BacklogGrid from "../components/BacklogGrid";
 import AddToFolder from "../components/AddToFolder";
 import CreateFolder from "../components/CreateFolder";
+import { usePopup } from "../contexts/PopupContext";
 
 const BacklogPage = () => {
+    const { activePopup, openPopup, closePopup } = usePopup();
+
+    // Helper function to close CreateFolder and reopen AddToFolder
+    const handleCloseCreateFolder = () => {
+        closePopup(); // close create folder popup
+        // small timeout so popup transition looks smooth
+        setTimeout(() => {
+            openPopup("addToFolder");
+        }, 150);
+    };
+
     return (
         <main>
             <article page-content="">
                 <section className="container">
-                    <BacklogHeader />
-
+                    <BacklogHeader
+                        onAddToFolder={() => openPopup("addToFolder")}
+                    />
                     <BacklogGrid />
                 </section>
             </article>
 
-            <AddToFolder />
-            <CreateFolder />
+            {activePopup === "addToFolder" && (
+                <AddToFolder
+                    onClose={closePopup}
+                    onCreateFolder={() => openPopup("createFolder")}
+                />
+            )}
+
+            {activePopup === "createFolder" && (
+                <CreateFolder onClose={handleCloseCreateFolder} />
+            )}
         </main>
     );
 };
