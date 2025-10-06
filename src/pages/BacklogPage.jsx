cbfebd59430a81ee02858556e46c4b61c6fb2ec0
Jose Abrego <joseabrego13@gmail.com>
2025-10-05 21:19:20 -0500

    Removed popup context, added it to components that need it instead. Removed unused params.

--- src/pages/BacklogPage.jsx
+++ src/pages/BacklogPage.jsx
@@ -3,41 +3,21 @@ import BacklogHeader from "../components/BacklogHeader";
 import BacklogGrid from "../components/BacklogGrid";
 import AddToFolder from "../components/AddToFolder";
 import CreateFolder from "../components/CreateFolder";
-import { usePopup } from "../contexts/PopupContext";
+import DeleteFolder from "../components/DeleteFolder";
 
 const BacklogPage = () => {
-    const { activePopup, openPopup, closePopup } = usePopup();
-
-    // Helper function to close CreateFolder and reopen AddToFolder
-    const handleCloseCreateFolder = () => {
-        closePopup(); // close create folder popup
-        // small timeout so popup transition looks smooth
-        setTimeout(() => {
-            openPopup("addToFolder");
-        }, 150);
-    };
-
     return (
         <main>
             <article page-content="">
                 <section className="container">
-                    <BacklogHeader
-                        onAddToFolder={() => openPopup("addToFolder")}
-                    />
+                    <BacklogHeader />
                     <BacklogGrid />
                 </section>
             </article>
 
-            {activePopup === "addToFolder" && (
-                <AddToFolder
-                    onClose={closePopup}
-                    onCreateFolder={() => openPopup("createFolder")}
-                />
-            )}
-
-            {activePopup === "createFolder" && (
-                <CreateFolder onClose={handleCloseCreateFolder} />
-            )}
+            <AddToFolder />
+            <CreateFolder />
+            <DeleteFolder />
         </main>
     );
 };
