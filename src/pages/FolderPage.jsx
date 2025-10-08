2c99b2f761e988ff9218237a0930fa0580862751
Jose Abrego <joseabrego13@gmail.com>
2025-10-08 05:55:38 -0500

    Temporarily loaded data from api to test folder grid.

--- src/pages/FolderPage.jsx
+++ src/pages/FolderPage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { imageBaseURL, API_KEY, fetchDataFromAPI } from "../utils/api";
+import LoadingOverlay from "../components/LoadingOverlay";
 import FolderHeader from "../components/FolderHeader";
 import FolderGrid from "../components/FolderGrid";
 import AddToFolder from "../components/AddToFolder";
@@ -7,20 +9,53 @@ import DeleteFolderItem from "../components/DeleteFolderItem";
 import Filter from "../components/Filter";
 
 const FolderPage = () => {
+    const [mediaList, setMediaList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // Folder movies
+    useEffect(() => {
+        const fetchMediaList = async () => {
+            setLoading(true);
+            const results = [];
+
+            try {
+                const res = await fetch(
+                    `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=pokemon&include_adult=false&language=en-US&page=1`
+                );
+                const data = await res.json();
+
+                setMediaList(data.results);
+            } catch (error) {
+                console.error("Error fetching data", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchMediaList();
+    }, []);
+
     return (
         <main>
-            <article page-content="">
-                <section className="container">
-                    <FolderHeader />
+            {loading && <LoadingOverlay />}
 
-                    <FolderGrid />
-                </section>
-            </article>
+            {!loading && (
+                <>
+                    <article page-content="">
+                        <section className="container">
+                            <FolderHeader />
+                            {mediaList.length > 0 && (
+                                <FolderGrid mediaList={mediaList} />
+                            )}
+                        </section>
+                    </article>
 
-            <AddToFolder />
-            <CreateFolder />
-            <DeleteFolderItem />
-            <Filter />
+                    <AddToFolder />
+                    <CreateFolder />
+                    <DeleteFolderItem />
+                    <Filter />
+                </>
+            )}
         </main>
     );
 };
