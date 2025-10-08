8d8cc487df43ce2884f1a2ee966f7be920a3a8f3
Jose Abrego <joseabrego13@gmail.com>
2025-10-08 01:16:00 -0500

    Can open filter pop up.

--- src/components/FolderHeader.jsx
+++ src/components/FolderHeader.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { usePopup } from "../contexts/PopupContext";
 
 const FolderHeader = () => {
+    const { activePopup, openPopup, closePopup } = usePopup();
+
     return (
         <>
             <div className="backlog-header">
@@ -21,7 +24,11 @@ const FolderHeader = () => {
                     placeholder="Search..."
                 />
 
-                <a className="btn-icon" onClick={null} id="backlog-add-btn">
+                <a
+                    className="btn-icon"
+                    onClick={() => openPopup("filter")}
+                    id="backlog-add-btn"
+                >
                     <svg className="material-icon" id="add-svg">
                         <use
                             xlinkHref={`${
