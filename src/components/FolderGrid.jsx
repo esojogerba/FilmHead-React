27732b833d61f42146859dc75208a8286c45c9fb
Jose Abrego <joseabrego13@gmail.com>
2025-10-08 05:54:57 -0500

    Temporarily loaded media to test folder grid cards.

--- src/components/FolderGrid.jsx
+++ src/components/FolderGrid.jsx
@@ -1,163 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePopup } from "../contexts/PopupContext";
+import MediaCard from "../components/MediaCard";
 import posterImg from "../assets/images/Blade Runner Poster.png";
 
-const FolderGrid = () => {
+const FolderGrid = ({ mediaList }) => {
     const { activePopup, openPopup, closePopup } = usePopup();
 
-    return (
-        <div className="grid-list folder-grid">
-            <div className="grid-card">
-                <figure className="poster-box grid-card-poster">
-                    <img
-                        src={posterImg}
-                        alt="Blade Runner"
-                        className="img-cover"
-                        loading="lazy"
-                    />
-                    <a
-                        className="grid-card-trash-btn"
-                        onClick={() => openPopup("deleteItem")}
-                    >
-                        <svg className="material-icon" id="grid-card-trash-svg">
-                            <use
-                                xlinkHref={`${
-                                    import.meta.env.BASE_URL
-                                }assets/images/icons.svg#trash`}
-                            />
-                        </svg>
-                    </a>
-                </figure>
-                <h4 className="media-card-title">Blade Runner</h4>
-                <div className="meta-list media-card-meta">
-                    <div className="meta-list">
-                        <div className="meta-item">1982</div>
-                    </div>
-                </div>
-                <a href="" className="card-btn" title="" onClick={null}></a>
-            </div>
+    if (mediaList != null) {
+        return (
+            <div className="grid-list folder-grid">
+                {mediaList.map((item) => {
+                    if (item.media_type != "person") {
+                        const mediaType =
+                            item.media_type === "tv" ? "show" : item.media_type;
 
-            <div className="grid-card">
-                <figure className="poster-box grid-card-poster">
-                    <img
-                        src={posterImg}
-                        alt="Blade Runner"
-                        className="img-cover"
-                        loading="lazy"
-                    />
-                    <a
-                        className="grid-card-trash-btn"
-                        onClick={() => openPopup("deleteItem")}
-                    >
-                        <svg className="material-icon" id="grid-card-trash-svg">
-                            <use
-                                xlinkHref={`${
-                                    import.meta.env.BASE_URL
-                                }assets/images/icons.svg#trash`}
+                        return (
+                            <MediaCard
+                                key={`${item.media_type}-${item.id}`}
+                                mediaData={item}
+                                type={`folder-${mediaType}`}
                             />
-                        </svg>
-                    </a>
-                </figure>
-                <h4 className="media-card-title">Blade Runner</h4>
-                <div className="meta-list media-card-meta">
-                    <div className="meta-list">
-                        <div className="meta-item">1982</div>
-                    </div>
-                </div>
-                <a href="" className="card-btn" title="" onClick={null}></a>
+                        );
+                    }
+                })}
             </div>
-
-            <div className="grid-card">
-                <figure className="poster-box grid-card-poster">
-                    <img
-                        src={posterImg}
-                        alt="Blade Runner"
-                        className="img-cover"
-                        loading="lazy"
-                    />
-                    <a
-                        className="grid-card-trash-btn"
-                        onClick={() => openPopup("deleteItem")}
-                    >
-                        <svg className="material-icon" id="grid-card-trash-svg">
-                            <use
-                                xlinkHref={`${
-                                    import.meta.env.BASE_URL
-                                }assets/images/icons.svg#trash`}
-                            />
-                        </svg>
-                    </a>
-                </figure>
-                <h4 className="media-card-title">Blade Runner</h4>
-                <div className="meta-list media-card-meta">
-                    <div className="meta-list">
-                        <div className="meta-item">1982</div>
-                    </div>
-                </div>
-                <a href="" className="card-btn" title="" onClick={null}></a>
-            </div>
-
-            <div className="grid-card">
-                <figure className="poster-box grid-card-poster">
-                    <img
-                        src={posterImg}
-                        alt="Blade Runner"
-                        className="img-cover"
-                        loading="lazy"
-                    />
-                    <a
-                        className="grid-card-trash-btn"
-                        onClick={() => openPopup("deleteItem")}
-                    >
-                        <svg className="material-icon" id="grid-card-trash-svg">
-                            <use
-                                xlinkHref={`${
-                                    import.meta.env.BASE_URL
-                                }assets/images/icons.svg#trash`}
-                            />
-                        </svg>
-                    </a>
-                </figure>
-                <h4 className="media-card-title">Blade Runner</h4>
-                <div className="meta-list media-card-meta">
-                    <div className="meta-list">
-                        <div className="meta-item">1982</div>
-                    </div>
-                </div>
-                <a href="" className="card-btn" title="" onClick={null}></a>
-            </div>
-
-            <div className="grid-card">
-                <figure className="poster-box grid-card-poster">
-                    <img
-                        src={posterImg}
-                        alt="Blade Runner"
-                        className="img-cover"
-                        loading="lazy"
-                    />
-                    <a
-                        className="grid-card-trash-btn"
-                        onClick={() => openPopup("deleteItem")}
-                    >
-                        <svg className="material-icon" id="grid-card-trash-svg">
-                            <use
-                                xlinkHref={`${
-                                    import.meta.env.BASE_URL
-                                }assets/images/icons.svg#trash`}
-                            />
-                        </svg>
-                    </a>
-                </figure>
-                <h4 className="media-card-title">Blade Runner</h4>
-                <div className="meta-list media-card-meta">
-                    <div className="meta-list">
-                        <div className="meta-item">1982</div>
-                    </div>
-                </div>
-                <a href="" className="card-btn" title="" onClick={null}></a>
-            </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default FolderGrid;
