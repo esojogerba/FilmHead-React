7a8079e48c0269c79c75f90f16d26d5f5e6e066a
Jose Abrego <joseabrego13@gmail.com>
2025-10-21 00:31:31 -0500

    Loads folders from backlog, creates folder component for each folder.

--- src/components/BacklogGrid.jsx
+++ src/components/BacklogGrid.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useBacklog } from "../contexts/BacklogContext";
 import Folder from "./Folder";
 
 const BacklogGrid = () => {
+    const { folders } = useBacklog();
+
+    // Sort folders from most recently modified
+    const sortedFolders = [...folders].sort(
+        (a, b) => new Date(b.lastModified) - new Date(a.lastModified)
+    );
+
     return (
         <div className="backlog-grid">
-            <Folder />
-            <Folder />
-            <Folder />
-            <Folder />
-            <Folder />
+            {sortedFolders.length > 0 ? (
+                sortedFolders.map((folder) => (
+                    <Folder key={folder.id} folder={folder} />
+                ))
+            ) : (
+                <p className="empty-backlog-message">No folders yet.</p>
+            )}
         </div>
     );
 };
