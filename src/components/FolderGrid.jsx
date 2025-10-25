fbfd0746d924c41dd43ade739d7eb1dd341060f2
Jose Abrego <joseabrego13@gmail.com>
2025-10-25 05:51:19 -0500

    Create media cards for each item inside of the folder.

--- src/components/FolderGrid.jsx
+++ src/components/FolderGrid.jsx
@@ -1,31 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { usePopup } from "../contexts/PopupContext";
+import React from "react";
 import MediaCard from "../components/MediaCard";
-import posterImg from "../assets/images/Blade Runner Poster.png";
 
 const FolderGrid = ({ mediaList }) => {
-    const { activePopup, openPopup, closePopup } = usePopup();
+    if (!mediaList || mediaList.length === 0) return null;
 
-    if (mediaList != null) {
-        return (
-            <div className="grid-list folder-grid">
-                {mediaList.map((item) => {
-                    if (item.media_type != "person") {
-                        const mediaType =
-                            item.media_type === "tv" ? "show" : item.media_type;
-
-                        return (
-                            <MediaCard
-                                key={`${item.media_type}-${item.id}`}
-                                mediaData={item}
-                                type={`folder-${mediaType}`}
-                            />
-                        );
-                    }
-                })}
-            </div>
-        );
-    }
+    return (
+        <div className="grid-list folder-grid">
+            {mediaList.map((item) => (
+                <MediaCard
+                    key={item.folderItemId}
+                    mediaData={{
+                        id: item.apiId,
+                        title: item.title,
+                        name: item.title,
+                        poster_path: item.posterPath,
+                        release_date: item.year?.toString() || "",
+                        first_air_date: item.year?.toString() || "",
+                    }}
+                    type={`folder-${item.mediaType}`}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default FolderGrid;
